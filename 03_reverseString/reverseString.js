af38f303635fdf3de69e49cec7a4a3fdc47f0a77
Steven <stevenmancans@gmail.com>
2022-05-28 13:24:57 +1000

    completed 01-04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split('');
+    reversedArray = wordArray.reverse();
+    return reversedArray.join('');
 };
 
 // Do not edit below this line

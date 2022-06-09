dfd32ef111473c685e91fff0155846ebdcec01bd
Steven <stevenmancans@gmail.com>
2022-06-09 16:30:19 +1000

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) {
+        return "OOPS";
+    } 
+    let fibArray = [];
+    for (let i = 0; i <= n; i++) {
+        if (i == 0) {
+            fibArray.push(0);
+        } else if (i == 1) {
+            fibArray.push(1);
+        } else {
+            fibArray.push((fibArray[i-1]+fibArray[i-2]));
+        }
+    };
+    return fibArray[n];
 };
 
 // Do not edit below this line

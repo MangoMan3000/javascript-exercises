977ca6a5951ef0c636703899a364415ef6f49b6a
Steven <stevenmancans@gmail.com>
2022-06-09 16:13:03 +1000

    completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const regex = /\w/;
+    let stringArray = string.split('');
+    let newStringArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+        if (regex.test(stringArray[i])) {
+            newStringArray.push(stringArray[i]);
+        };
+    };
+    let checkString = newStringArray.join('').toLowerCase();
+    let reverseString = newStringArray.reverse().join('').toLowerCase();
+    if (checkString == reverseString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

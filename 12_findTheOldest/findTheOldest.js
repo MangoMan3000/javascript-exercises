a90cc1d58b7f461ff2415edb4f01c129c35f4d0c
Steven <stevenmancans@gmail.com>
2022-06-10 17:51:04 +1000

    completed findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for (person of people) {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = 2022;
+        }
+    }
+    const ordered = people.sort(function(personA, personB) {
+        if ((personA.yearOfDeath - personA.yearOfBirth) > (personB.yearOfDeath - personB.yearOfBirth)){
+            return -1;
+        } else {
+            return 1;
+        }
+    });
+    return ordered[0];
 };
 
 // Do not edit below this line
